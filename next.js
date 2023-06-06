f916bbf9c9080010c167a3631cf188cbf0fd0958
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-06 17:24:33 +0700

    Update next.js

--- next.js
+++ next.js
@@ -1,6 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-
 module.exports = {
   async headers() {
     return [
@@ -16,19 +13,11 @@ module.exports = {
     ];
   },
   async rewrites() {
-    const serverFilePath = path.join(__dirname, 'server.js');
-    const isServerFileExists = fs.existsSync(serverFilePath);
-
-    if (isServerFileExists) {
-      return [
-        {
-          source: '/(.*)',
-          destination: '/server.js',
-        },
-      ];
-    } else {
-      console.error('File server.js tidak ditemukan');
-      return [];
-    }
+    return [
+      {
+        source: '/(.*)',
+        destination: '/server.js',
+      },
+    ];
   },
 };
