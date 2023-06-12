5ff9941f4b44065bf1db83b381f89ebf1e19b87a
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-13 01:24:32 +0700

    Update base.js

--- linked/base.js
+++ linked/base.js
@@ -99,49 +99,51 @@ document.title = updatedTitle;
  head.appendChild(linkStylesheet);
 
   function loadScripts(scripts, callback) {
-    var loadedCount = 0;
+  var loadedCount = 0;
 
-    function scriptLoaded() {
-      loadedCount++;
-      if (loadedCount === scripts.length) {
-        callback();
-      }
-    }
-
-    function loadScript(script) {
-      var scriptElement = document.createElement("script");
-      scriptElement.src = script;
-      scriptElement.onload = scriptLoaded;
-      document.head.appendChild(scriptElement);
-    }
-
-    for (var i = 0; i < scripts.length; i++) {
-      loadScript(scripts[i]);
+  function scriptLoaded() {
+    loadedCount++;
+    if (loadedCount === scripts.length) {
+      callback();
     }
   }
 
-  var scriptsToLoad = [
-    "/linked/pembuka.js",
-    "/linked/penutup.js",
-    "/linked/log.js",
-    "/linked/favicon.js",
-    "/linked/bg_random_color.js",
-    "/linked/google.js",
-    "/linked/teks-kedip.js",
-    "/linked/ip-address.js",
-    "/linked/update.js",
-    "/linked/teks-config.js",
-    "/linked/speed.js",
-    "/linked/komenwa.js",
-    "/linked/jam-digital.js",
-    "/linked/jam-analog.js",
-    "/linked/names.js"
-  ];
+  function loadScript(script) {
+    var scriptElement = document.createElement("script");
+    scriptElement.src = script;
+    scriptElement.onload = scriptLoaded;
+    document.head.appendChild(scriptElement);
+  }
 
-  window.onload = function() {
-    loadScripts(scriptsToLoad, function() {
-    });
-  };
+  for (var i = 0; i < scripts.length; i++) {
+    loadScript(scripts[i]);
+  }
+}
+
+var scriptsToLoad = [
+  "linked/favicon.js",
+  "linked/bg_random_color.js",
+  "linked/base.js",
+  "linked/pembuka.js",
+  "linked/penutup.js",
+  "linked/script.js",
+  "linked/google.js",
+  "linked/teks-kedip.js",
+  "linked/ip-address.js",
+  "linked/update.js",
+  "linked/teks-config.js",
+  "linked/speed.js",
+  "linked/komenwa.js",
+  "linked/jam-digital.js",
+  "linked/jam-analog.js",
+  "linked/names.js"
+];
+
+window.onload = function() {
+  loadScripts(scriptsToLoad, function() {
+    // Semua skrip telah dimuat
+  });
+};
 
 // pembuka an
 const pembukaHTML = `   
