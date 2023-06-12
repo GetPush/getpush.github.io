33669a44975ab59a33f80f9c3964790e2922f668
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-12 22:53:21 +0700

    Update bg_random_color.js

--- linked/bg_random_color.js
+++ linked/bg_random_color.js
@@ -1,4 +1,3 @@
-
         // Fungsi untuk mendapatkan warna acak dalam format RGB
         function getRandomColor() {
             var r = Math.floor(Math.random() * 256);
@@ -7,33 +6,24 @@
             return 'rgb(' + r + ',' + g + ',' + b + ')';
         }
 
-        // Fungsi untuk memeriksa apakah warna sebelumnya tersimpan
         function getSavedColor() {
             return localStorage.getItem('backgroundColor');
         }
 
-        // Fungsi untuk mengatur warna latar belakang
         function setBackgroundColor(color) {
             document.body.style.backgroundColor = color;
-            localStorage.setItem('backgroundColor', color); // Simpan warna baru di penyimpanan lokal
+            localStorage.setItem('backgroundColor', color); 
         }
 
-        // Hapus warna yang tersimpan saat halaman diperbarui
         function clearSavedColor() {
             localStorage.removeItem('backgroundColor');
         }
-
-        // Periksa apakah warna sebelumnya tersimpan
         var savedColor = getSavedColor();
 
-        // Jika warna tersimpan, gunakan warna tersebut
         if (savedColor) {
             setBackgroundColor(savedColor);
         } else {
-            // Jika warna belum tersimpan, atur warna acak baru
             var randomColor = getRandomColor();
             setBackgroundColor(randomColor);
         }
-
-        // Hapus warna yang tersimpan saat halaman diperbarui
         clearSavedColor();
