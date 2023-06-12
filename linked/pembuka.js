2851318662aada4e52343e321c86c5f2828d521f
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-12 16:02:20 +0700

    Add files via upload

---
+++ linked/pembuka.js
@@ -0,0 +1,59 @@
+  const pembukaHTML = ` 
+    <header>  
+     <div class="nav-row-1">  
+      <nav>  
+       <ul class="nav-container">  
+        <h1 class="title"></h1>  
+        <li>  
+         <a href="/about/grup-telegram.html">Telegram</a>  
+        </li>  
+        <li>  
+         <a href="https://pena-internet.blogspot.com/">Pena Internet</a>  
+        </li>  
+        <li>  
+         <a href="/page/ssh/direct.html">Create SSH</a>  
+        </li>  
+        <li>  
+         <a href="/about/sosmed.html">Sosmed</a>  
+        </li>  
+        <li>  
+         <a href="/linked/images/donasi.png">Donasi</a>  
+        </li>  
+        <li>  
+         <a href="https://apppintar.blogspot.com/">Blog</a>  
+        </li>  
+       </ul>  
+      </nav>  
+     </div>  
+     <div class="nav-row-2">  
+      <nav>  
+       <ul class="nav-container">  
+        <h1 class="title"></h1>  
+        <li>  
+         <a href="/page/home.html">Beranda</a>  
+        </li>  
+        <li>  
+         <a href="/page/tutorial.html">Tutorial</a>  
+        </li>  
+        <li>  
+         <a href="/page/apps.html">Aplikasi</a>  
+        </li>  
+        <li>  
+         <a href="https://www.youtube.com/channel/UCeMezZCLKzcpQyYFSiW6DCA?sub_confirmation=1">YouTube</a>  
+        </li>  
+        <li>  
+         <a href="/page/dana-kaget/direct.html">Dana Kaget</a>  
+        </li>  
+        <li>  
+         <a href="https://apppintar.blogspot.com/">Selengkapnya =></a>  
+        </li>  
+       </ul>  
+      </nav>  
+     </div>  
+    </header>  
+    <h1>ألسلام عليهم ورحمة الله وبركاته</h1>  
+    <div id="teks1">Halo, selamat datang!</div> 
+   `; 
+  
+   const pembukaElement = document.getElementById("pembuka"); 
+   pembukaElement.innerHTML = pembukaHTML;
