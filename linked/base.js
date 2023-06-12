b6735b002530c333818a047be77ec84165cbbce9
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-12 22:54:04 +0700

    Update base.js

--- linked/base.js
+++ linked/base.js
@@ -205,6 +205,36 @@ const pembukaHTML = `
      } 
    });
 
+        // Fungsi untuk mendapatkan warna acak dalam format RGB 
+         function getRandomColor() { 
+             var r = Math.floor(Math.random() * 256); 
+             var g = Math.floor(Math.random() * 256); 
+             var b = Math.floor(Math.random() * 256); 
+             return 'rgb(' + r + ',' + g + ',' + b + ')'; 
+         } 
+  
+         function getSavedColor() { 
+             return localStorage.getItem('backgroundColor'); 
+         } 
+  
+         function setBackgroundColor(color) { 
+             document.body.style.backgroundColor = color; 
+             localStorage.setItem('backgroundColor', color);  
+         } 
+  
+         function clearSavedColor() { 
+             localStorage.removeItem('backgroundColor'); 
+         } 
+         var savedColor = getSavedColor(); 
+  
+         if (savedColor) { 
+             setBackgroundColor(savedColor); 
+         } else { 
+             var randomColor = getRandomColor(); 
+             setBackgroundColor(randomColor); 
+         } 
+         clearSavedColor();
+
 // Metode caching
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'data.json', true);
