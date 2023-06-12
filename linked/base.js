c1922387b359cc865b20ea6dd6947aa5352a1f07
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-12 22:08:10 +0700

    Update base.js

--- linked/base.js
+++ linked/base.js
@@ -98,6 +98,29 @@ document.title = updatedTitle;
  head.appendChild(linkFavicon); 
  head.appendChild(linkStylesheet);
 
+function loadScript(path) { 
+   var script = document.createElement('script'); 
+   script.src = path; 
+   document.head.appendChild(script); 
+ } 
+  
+ loadScript("linked/favicon.js"); 
+ loadScript("linked/bg_random_color.js"); 
+ loadScript("linked/base.js"); 
+ loadScript("linked/pembuka.js"); 
+ loadScript("linked/penutup.js"); 
+ loadScript("linked/script.js"); 
+ loadScript("linked/google.js"); 
+ loadScript("linked/teks-kedip.js"); 
+ loadScript("linked/ip-address.js"); 
+ loadScript("linked/update.js"); 
+ loadScript("linked/teks-config.js"); 
+ loadScript("linked/speed.js"); 
+ loadScript("linked/komenwa.js"); 
+ loadScript("linked/jam-digital.js"); 
+ loadScript("linked/jam-analog.js"); 
+ loadScript("linked/names.js");
+
 // Metode caching
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'data.json', true);
