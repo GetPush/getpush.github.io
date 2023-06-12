c2dd570acdafee39f1e7d1aadefea34469b95db5
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-12 20:09:06 +0700

    Update script.js

--- linked/script.js
+++ linked/script.js
@@ -316,3 +316,34 @@ function autoScroll() {
 }
 
 setInterval(autoScroll, 500); // Mengatur interval waktu scroll, contohnya 100 milidetik atau 0.1 detik
+
+// menampilkan kecepatan internet
+function measureInternetSpeed(speedElementId) { 
+   var speedElement = document.getElementById(speedElementId); 
+  
+   var startTime, endTime; 
+   var download = new Image(); 
+   download.onload = function() { 
+     endTime = (new Date()).getTime(); 
+     showSpeed(); 
+   }; 
+  
+   download.onerror = function(err, msg) { 
+     console.log("Download error", err, msg); 
+     showSpeed(); 
+   }; 
+  
+   startTime = (new Date()).getTime(); 
+   var cacheBuster = "?nnn=" + startTime; 
+   download.src = "/display/media/images/bg.jpg" + cacheBuster; 
+  
+   function showSpeed() { 
+     var duration = (endTime - startTime) / 1000; 
+     var bitsLoaded = 1.14 * 8; // Ukuran gambar dalam bit 
+     var speedBps = (bitsLoaded / duration).toFixed(2); 
+     var speedKbps = (speedBps / 1024).toFixed(2); 
+     var speedMbps = (speedKbps / 1024).toFixed(2); 
+  
+     speedElement.innerHTML = "Kecepatan Internet Anda: " + speedMbps + " Mbps"; 
+   } 
+ }
