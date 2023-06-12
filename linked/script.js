98ad4732539837514c61db88d0be482d71a7b3a1
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-12 19:57:20 +0700

    Update script.js

--- linked/script.js
+++ linked/script.js
@@ -121,3 +121,15 @@ function setFaviconColor(color) {
 
 // Set random color as favicon
 setFaviconColor(getRandomColor());
+
+// cek ip address
+fetch('https://api.ipify.org/?format=json') 
+         .then(response => response.json()) 
+         .then(data => { 
+           const ipAddress = data.ip; 
+           const ipAddressElement = document.getElementById('ip-address'); 
+           ipAddressElement.textContent = ipAddress; 
+         }) 
+         .catch(error => { 
+           console.error('Terjadi kesalahan saat mengambil data IP', error); 
+         });
