191109d4b0d4c0dd3897bf061c748416f4fea612
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-12 21:41:42 +0700

    Update script.js

--- linked/script.js
+++ linked/script.js
@@ -1,21 +1,27 @@
-    const scripts = [
-      "/linked/favicon.js",
-      "/linked/bg_random_color.js",
-      "/linked/pembuka.js",
-      "/linked/penutup.js",
-      "/linked/base.js",
-      "/linked/google.js",
-      "/linked/teks-kedip.js",
-      "/linked/ip-address.js",
-      "/linked/update.js",
-      "/linked/teks-config.js",
-      "/linked/speed.js",
-      "/linked/komenwa.js",
-      "/linked/jam-digital.js",
-      "/linked/jam-analog.js",
-      "/linked/names.js"
-    ];
-
-    scripts.forEach(script => {
-      document.write(`<script src="${script}"></script>`);
-    });
+    const scripts = [ 
+       "linked/favicon.js", 
+       "linked/bg_random_color.js", 
+       "linked/base.js", 
+       "linked/pembuka.js", 
+       "linked/penutup.js", 
+       "linked/script.js", 
+       "linked/google.js", 
+       "linked/teks-kedip.js", 
+       "linked/ip-address.js", 
+       "linked/update.js", 
+       "linked/teks-config.js", 
+       "linked/speed.js", 
+       "linked/komenwa.js", 
+       "linked/jam-digital.js", 
+       "linked/jam-analog.js", 
+       "linked/names.js" 
+     ]; 
+  
+     function loadScripts() { 
+       scripts.forEach(function(script) { 
+         var scriptElement = document.createElement('script'); 
+         scriptElement.src = script; 
+         document.head.appendChild(scriptElement); 
+       }); 
+     } 
+     loadScripts();
