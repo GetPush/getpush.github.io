655c9c3f91d81b69c3ed8eaa6e302ea10bb68551
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-12 20:15:17 +0700

    Update script.js

--- linked/script.js
+++ linked/script.js
@@ -431,3 +431,102 @@ document.getElementById("comment-form").addEventListener("submit", function(even
      // Mengosongkan kolom komentar setelah pengiriman 
      document.getElementById("comment-input").value = ""; 
  });
+
+// penutup konten website
+  const penutupHTML = ` 
+ <br> 
+  <p>Buy Me ☕</p>   
+   <h3>QR Code Ini Support Untuk Semua Dompet</h3>   
+   <br>   
+       <img src="/linked/images/QRcode.png" alt="QR Code" width="400" height="400">  
+ <div> 
+ <br> 
+    <p>Saat Ini Di Update Oleh: [ <span id="randomNames2" style="font-size: 35px;"></span> ]</p>  
+ <style> 
+ h1 { 
+     text-align: center; 
+ } 
+  
+ #comment-form { 
+     text-align: center; 
+     margin-top: 20px; 
+ } 
+  
+ #comment-input { 
+     width: 300px; 
+     height: 100px; 
+     resize: none; 
+ } 
+  
+ button { 
+     margin-top: 10px; 
+ } 
+ </style> 
+     <h1>komentar</h1> 
+     <form id="comment-form"> 
+         <textarea id="comment-input" placeholder="Masukkan komentar Anda"></textarea> 
+         <button type="submit">Kirim</button> 
+     </form> 
+ </div> 
+     <br> 
+     <!-- penutup konten --> 
+     <h1 style="font-size: 17px; font-weight: 400;"> untuk versi Prem silahkan contact admin kami !!!</h1> 
+     <br> 
+     <div style="text-align: center;"> 
+       <a href="mailto:munirggplay@gmail.com" style="display: inline-block; margin-right: 10px;"> 
+         <img alt="Gmail Logo" height="32" src="https://www.gstatic.com/images/icons/material/product/2x/gmail_64dp.png" width="32" /> Email 
+       </a> 
+       <a href="https://wa.me/6283838367172" rel="noopener noreferrer" target="_blank" style="display: inline-block; margin-right: 10px;"> 
+         <img alt="WhatsApp logo" height="30" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png" width="30" /> WhatsApp 
+       </a> 
+       <a href="https://t.me/telgruptoppayofficial" target="_blank" style="display: inline-block;"> 
+         <img alt="Telegram" height="30" src="https://telegram.org/img/t_logo.svg" width="30" /> Telegram 
+       </a> 
+     </div> 
+     <h3 style="color: #3e005d;"> 
+       <br /> We will revert you as soon as possible...! 
+     </h3> 
+     <h1 style="font-size: medium; font-weight: 400; text-align: center;"> 
+       <p style="color: #3e005d;"> Thank you for contacting us! <br /> 
+         <b>Have a great day</b> 
+       </p> 
+     </h1> 
+     <p> 
+       <strong>TERIMAKASIH</strong> 
+     </p> 
+     </div> 
+     <div class="nav-row-3"> 
+       <ul> 
+         <li> 
+           <a href="/info/disclaimer.html">Disclaimer</a> 
+         </li> 
+         <li> 
+           <a href="/info/about.html">About</a> 
+         </li> 
+         <li> 
+           <a href="/public/sitemap.xml">sitemap</a> 
+         </li> 
+         <li> 
+           <a href="/info/contacts.html">Contacts</a> 
+         </li> 
+         <li> 
+           <a href="/info/privacy-policy.html">Privacy Policy</a> 
+         </li> 
+         <li> 
+           <a href="/info/terms-and-conditions.html">Terms And Conditions</a> 
+         </li> 
+       </ul> 
+     </div> 
+     <div class="footer"> 
+       <footer> 
+         Copyright 2023 / website <a href="https://wa.me/6283838367172">template</a> 
+       </footer> 
+     </div> 
+   `; 
+  
+       window.addEventListener("DOMContentLoaded", function () {  
+      var penutupElement = document.getElementById("penutup");  
+      if (penutupElement) {  
+        penutupElement.innerHTML = penutupHTML;  
+      }  
+    });
