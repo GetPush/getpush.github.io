e9936f46dd35c406d6a15386e28c893847c8920b
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-12 19:19:21 +0700

    Update script.js

--- linked/script.js
+++ linked/script.js
@@ -18,22 +18,23 @@ function loadScripts(scripts, callback) {
 }
 
 var scripts = [
-  "/linked/favicon.js", 
-   "/linked/bg_random_color.js", 
-   "/linked/pembuka.js", 
-   "/linked/penutup.js", 
-   "/linked/google.js", 
-   "/linked/teks-kedip.js", 
-   "/linked/ip-address.js", 
-   "/linked/update.js", 
-   "/linked/teks-config.js", 
-   "/linked/speed.js", 
-   "/linked/komenwa.js", 
-   "/linked/jam-digital.js", 
-   "/linked/jam-analog.js", 
-   "/linked/names.js",
-   "/linked/log.js"
+    "/linked/favicon.js",
+    "/linked/bg_random_color.js",
+    "/linked/pembuka.js",
+    "/linked/penutup.js",
+    "/linked/google.js",
+    "/linked/teks-kedip.js",
+    "/linked/ip-address.js",
+    "/linked/update.js",
+    "/linked/teks-config.js",
+    "/linked/speed.js",
+    "/linked/komenwa.js",
+    "/linked/jam-digital.js",
+    "/linked/jam-analog.js",
+    "/linked/names.js",
+    "/linked/log.js"
 ];
 
-loadScripts(scripts, function() {
+loadScripts(scripts, function () {
+    // Kode ini akan dijalankan setelah semua skrip berhasil dimuat
 });
