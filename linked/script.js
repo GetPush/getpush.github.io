066b62f8ccc2d105dd9529a715a81080736e1f11
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-12 20:14:13 +0700

    Update script.js

--- linked/script.js
+++ linked/script.js
@@ -411,3 +411,23 @@ window.addEventListener('DOMContentLoaded', (event) => {
   
    //<p id="created"></p> 
    //<p id="expired"></p>
+
+// agar komentar website masuk ke WhatsApp
+document.getElementById("comment-form").addEventListener("submit", function(event) { 
+     event.preventDefault(); // Mencegah pengiriman formulir secara default 
+  
+     var comment = document.getElementById("comment-input").value; 
+  
+     // Ganti nomor WhatsApp dan pesan sesuai dengan kebutuhan Anda 
+     var phoneNumber = "082311529002"; 
+     var message = "Komentar: " + comment; 
+  
+     // Membuat link WhatsApp dengan nomor dan pesan 
+     var whatsappLink = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodeURIComponent(message); 
+  
+     // Membuka link WhatsApp di tab baru 
+     window.open(whatsappLink, "_blank"); 
+  
+     // Mengosongkan kolom komentar setelah pengiriman 
+     document.getElementById("comment-input").value = ""; 
+ });
