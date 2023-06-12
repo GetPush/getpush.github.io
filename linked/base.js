ed0826169dcb3b182de0f358bb3649863165999e
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-13 01:34:27 +0700

    Update base.js

--- linked/base.js
+++ linked/base.js
@@ -99,8 +99,7 @@ document.title = updatedTitle;
  head.appendChild(linkStylesheet);
 
 <script src="linked/favicon.js" async></script>
-<script src="linked/bg_random_color.js"></script>
-<script src="linked/base.js" async></script>
+<script src="linked/bg_random_color.js" async></script>
 <script src="linked/pembuka.js" async></script>
 <script src="linked/penutup.js" async></script>
 <script src="linked/script.js" async></script>
