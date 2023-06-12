d7626d9ac3c4cbd9a6c37e8569345c6c3083aa60
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-12 21:56:12 +0700

    Update script.js

--- linked/script.js
+++ linked/script.js
@@ -1,27 +1,37 @@
-    const scripts = [ 
-       "linked/favicon.js", 
-       "linked/bg_random_color.js", 
-       "linked/base.js", 
-       "linked/pembuka.js", 
-       "linked/penutup.js", 
-       "linked/script.js", 
-       "linked/google.js", 
-       "linked/teks-kedip.js", 
-       "linked/ip-address.js", 
-       "linked/update.js", 
-       "linked/teks-config.js", 
-       "linked/speed.js", 
-       "linked/komenwa.js", 
-       "linked/jam-digital.js", 
-       "linked/jam-analog.js", 
-       "linked/names.js" 
-     ]; 
+const scripts = [ 
+   "linked/favicon.js", 
+   "linked/bg_random_color.js", 
+   "linked/base.js", 
+   "linked/pembuka.js", 
+   "linked/penutup.js", 
+   "linked/script.js", 
+   "linked/google.js", 
+   "linked/teks-kedip.js", 
+   "linked/ip-address.js", 
+   "linked/update.js", 
+   "linked/teks-config.js", 
+   "linked/speed.js", 
+   "linked/komenwa.js", 
+   "linked/jam-digital.js", 
+   "linked/jam-analog.js", 
+   "linked/names.js" 
+ ]; 
   
-     function loadScripts() { 
-       scripts.forEach(function(script) { 
-         var scriptElement = document.createElement('script'); 
-         scriptElement.src = script; 
-         document.head.appendChild(scriptElement); 
-       }); 
-     } 
-     loadScripts();
+ function loadScript(url) { 
+   return new Promise((resolve, reject) => { 
+     const script = document.createElement('script'); 
+     script.src = url; 
+     script.onload = resolve; 
+     script.onerror = reject; 
+     document.head.appendChild(script); 
+   }); 
+ } 
+  
+ async function loadScripts() { 
+   for (const script of scripts) { 
+     await loadScript(script).catch(error => { 
+     }); 
+   } 
+ } 
+  
+ loadScripts();
