a29f8169e6a3d899afc6b6279c8deed1413d5bb3
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-12 22:48:56 +0700

    Update base.js

--- linked/base.js
+++ linked/base.js
@@ -144,6 +144,67 @@ document.title = updatedTitle;
     });
   };
 
+// pembuka an
+const pembukaHTML = `   
+   <div class="nav-row-1">    
+     <nav>    
+       <ul class="nav-container">    
+         <h1 class="title"></h1>    
+         <li>    
+           <a href="/about/grup-telegram.html">Telegram</a>    
+         </li>    
+         <li>    
+           <a href="https://pena-internet.blogspot.com/">Pena Internet</a>    
+         </li>    
+         <li>    
+           <a href="/page/ssh/direct.html">Create SSH</a>    
+         </li>    
+         <li>    
+           <a href="/about/sosmed.html">Sosmed</a>    
+         </li>    
+         <li>    
+           <a href="/linked/images/donasi.png">Donasi</a>    
+         </li>    
+         <li>    
+           <a href="https://apppintar.blogspot.com/">Blog</a>    
+         </li>    
+       </ul>    
+     </nav>    
+   </div>    
+   <div class="nav-row-2">    
+     <nav>    
+       <ul class="nav-container">    
+         <h1 class="title"></h1>    
+         <li>    
+           <a href="/page/home.html">Beranda</a>    
+         </li>    
+         <li>    
+           <a href="/page/tutorial.html">Tutorial</a>    
+         </li>    
+         <li>    
+           <a href="/page/apps.html">Aplikasi</a>    
+         </li>    
+         <li>    
+           <a href="https://www.youtube.com/channel/UCeMezZCLKzcpQyYFSiW6DCA?sub_confirmation=1">YouTube</a>    
+         </li>    
+         <li>    
+           <a href="/page/dana-kaget/direct.html">Dana Kaget</a>    
+         </li>    
+         <li>    
+           <a href="https://apppintar.blogspot.com/">Selengkapnya =></a>    
+         </li>    
+       </ul>    
+     </nav>    
+   </div>  
+ `; 
+  
+   window.addEventListener("DOMContentLoaded", function () { 
+     var pembukaElement = document.getElementById("pembuka"); 
+     if (pembukaElement) { 
+       pembukaElement.innerHTML = pembukaHTML; 
+     } 
+   });
+
 // Metode caching
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'data.json', true);
