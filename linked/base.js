a8c8affb0c9f9027360da104a0f0b2960af4c2d0
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-12 21:55:45 +0700

    Update base.js

--- linked/base.js
+++ linked/base.js
@@ -129,34 +129,43 @@ document.title = updatedTitle;
  head.appendChild(linkFavicon); 
  head.appendChild(linkStylesheet);
 
+const scripts = [
+  "linked/favicon.js",
+  "linked/bg_random_color.js",
+  "linked/base.js",
+  "linked/pembuka.js",
+  "linked/penutup.js",
+  "linked/script.js",
+  "linked/google.js",
+  "linked/teks-kedip.js",
+  "linked/ip-address.js",
+  "linked/update.js",
+  "linked/teks-config.js",
+  "linked/speed.js",
+  "linked/komenwa.js",
+  "linked/jam-digital.js",
+  "linked/jam-analog.js",
+  "linked/names.js"
+];
 
-    const scripts = [ 
-       "linked/favicon.js", 
-       "linked/bg_random_color.js", 
-       "linked/base.js", 
-       "linked/pembuka.js", 
-       "linked/penutup.js", 
-       "linked/script.js", 
-       "linked/google.js", 
-       "linked/teks-kedip.js", 
-       "linked/ip-address.js", 
-       "linked/update.js", 
-       "linked/teks-config.js", 
-       "linked/speed.js", 
-       "linked/komenwa.js", 
-       "linked/jam-digital.js", 
-       "linked/jam-analog.js", 
-       "linked/names.js" 
-     ]; 
-  
-     function loadScripts() { 
-       scripts.forEach(function(script) { 
-         var scriptElement = document.createElement('script'); 
-         scriptElement.src = script; 
-         document.head.appendChild(scriptElement); 
-       }); 
-     } 
-     loadScripts();
+function loadScript(url) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = url;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.appendChild(script);
+  });
+}
+
+async function loadScripts() {
+  for (const script of scripts) {
+    await loadScript(script).catch(error => {
+    });
+  }
+}
+
+loadScripts();
 
 // Metode caching
 const xhr = new XMLHttpRequest();
