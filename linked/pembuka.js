f014ba1c5a9e53da7aa6a3104615a10383bb8d09
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-13 13:32:24 +0700

    Update pembuka.js

--- linked/pembuka.js
+++ linked/pembuka.js
@@ -1,54 +1,12 @@
 const pembukaHTML = `  
-  <div class="nav-row-1">   
-    <nav>   
-      <ul class="nav-container">   
-        <h1 class="title"></h1>   
-        <li>   
-          <a href="/about/grup-telegram.html">Telegram</a>   
-        </li>   
-        <li>   
-          <a href="https://pena-internet.blogspot.com/">Pena Internet</a>   
-        </li>   
-        <li>   
-          <a href="/page/ssh/direct.html">Create SSH</a>   
-        </li>   
-        <li>   
-          <a href="/about/sosmed.html">Sosmed</a>   
-        </li>   
-        <li>   
-          <a href="/linked/images/donasi.png">Donasi</a>   
-        </li>   
-        <li>   
-          <a href="https://apppintar.blogspot.com/">Blog</a>   
-        </li>   
-      </ul>   
-    </nav>   
-  </div>   
-  <div class="nav-row-2">   
-    <nav>   
-      <ul class="nav-container">   
-        <h1 class="title"></h1>   
-        <li>   
-          <a href="/page/home.html">Beranda</a>   
-        </li>   
-        <li>   
-          <a href="/page/tutorial.html">Tutorial</a>   
-        </li>   
-        <li>   
-          <a href="/page/apps.html">Aplikasi</a>   
-        </li>   
-        <li>   
-          <a href="https://www.youtube.com/channel/UCeMezZCLKzcpQyYFSiW6DCA?sub_confirmation=1">YouTube</a>   
-        </li>   
-        <li>   
-          <a href="/page/dana-kaget/direct.html">Dana Kaget</a>   
-        </li>   
-        <li>   
-          <a href="https://apppintar.blogspot.com/">Selengkapnya =></a>   
-        </li>   
-      </ul>   
-    </nav>   
-  </div> 
+   <div id="teks1">Halo, selamat datang!</div>  
+    <div style="text-align: left;">  
+     <p>• Sebagai umat manusia, kita semua saling membutuhkan, saling membantu, dan saling mendukung.</p>  
+     <div>• Semoga yang mengunjungi website ini, pada hari <span style="display: inline;">  
+       <div id="jam-sambutan" style="display: inline;"></div>  
+      </span> selalu diberikan kesehatan, keselamatan, dan kebahagiaan dalam hidupnya. </div>  
+     <p>• Dan semoga apa yang Anda cari dapat ditemukan di website ini, serta bermanfaat dan bisa membawa manfaat untuk diri sendiri dan orang lain.</p>  
+    </div> 
 `;
 
   window.addEventListener("DOMContentLoaded", function () {
