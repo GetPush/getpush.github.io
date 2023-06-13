7caa164dc6f689d8b53e1670f847224be812c942
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-13 13:30:08 +0700

    Update base.js

--- linked/base.js
+++ linked/base.js
@@ -98,6 +98,67 @@ document.title = updatedTitle;
  head.appendChild(linkFavicon); 
  head.appendChild(linkStylesheet);
 
+// header
+const headerHTML = `   
+   <div class="nav-row-1">    
+     <nav>    
+       <ul class="nav-container">    
+         <h1 class="title"></h1>    
+         <li>    
+           <a href="/about/grup-telegram.html">Telegram</a>    
+         </li>    
+         <li>    
+           <a href="https://pena-internet.blogspot.com/">Pena Internet</a>    
+         </li>    
+         <li>    
+           <a href="/page/ssh/direct.html">Create SSH</a>    
+         </li>    
+         <li>    
+           <a href="/about/sosmed.html">Sosmed</a>    
+         </li>    
+         <li>    
+           <a href="/linked/images/donasi.png">Donasi</a>    
+         </li>    
+         <li>    
+           <a href="https://apppintar.blogspot.com/">Blog</a>    
+         </li>    
+       </ul>    
+     </nav>    
+   </div>    
+   <div class="nav-row-2">    
+     <nav>    
+       <ul class="nav-container">    
+         <h1 class="title"></h1>    
+         <li>    
+           <a href="/page/home.html">Beranda</a>    
+         </li>    
+         <li>    
+           <a href="/page/tutorial.html">Tutorial</a>    
+         </li>    
+         <li>    
+           <a href="/page/apps.html">Aplikasi</a>    
+         </li>    
+         <li>    
+           <a href="https://www.youtube.com/channel/UCeMezZCLKzcpQyYFSiW6DCA?sub_confirmation=1">YouTube</a>    
+         </li>    
+         <li>    
+           <a href="/page/dana-kaget/direct.html">Dana Kaget</a>    
+         </li>    
+         <li>    
+           <a href="https://apppintar.blogspot.com/">Selengkapnya =></a>    
+         </li>    
+       </ul>    
+     </nav>    
+   </div>  
+ `; 
+  
+   window.addEventListener("DOMContentLoaded", function () { 
+     var headerElement = document.getElementById("header"); 
+     if (headerElement) { 
+       headerElement.innerHTML = headerHTML; 
+     } 
+   });
+
    function loadScripts(scripts, callback) {  
       var loadedCount = 0;  
   
