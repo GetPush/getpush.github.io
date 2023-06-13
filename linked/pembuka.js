2142c761b221d994379ad740c59913bc0c15336f
MyHost <126472922+botstart@users.noreply.github.com>
2023-06-13 13:38:21 +0700

    Update pembuka.js

--- linked/pembuka.js
+++ linked/pembuka.js
@@ -1,14 +1,14 @@
 const pembukaHTML = `  
  <h1>ألسلام عليهم ورحمة الله وبركاته</h1> 
-  <br>
-   <div id="teks1">Halo, selamat datang!</div>  
+ <div id="teks1">Halo, selamat datang!</div>  
     <div style="text-align: left;">  
      <p>• Sebagai umat manusia, kita semua saling membutuhkan, saling membantu, dan saling mendukung.</p>  
-     <div>• Semoga yang mengunjungi website ini, pada hari <span style="display: inline;">  
+<div>• Semoga yang mengunjungi website ini, pada hari <span style="display: inline;">  
        <div id="jam-sambutan" style="display: inline;"></div>  
       </span> selalu diberikan kesehatan, keselamatan, dan kebahagiaan dalam hidupnya. </div>  
      <p>• Dan semoga apa yang Anda cari dapat ditemukan di website ini, serta bermanfaat dan bisa membawa manfaat untuk diri sendiri dan orang lain.</p>  
-    </div> 
+</div>
+<br>
 `;
 
   window.addEventListener("DOMContentLoaded", function () {
