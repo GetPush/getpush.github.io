b3a6bc7c977f9d5775039a6c96b2aefe013f5dde
MyHost <126472922+botstart@users.noreply.github.com>
2023-10-08 09:16:46 +0700

    Update penutup.js

--- linked/penutup.js
+++ linked/penutup.js
@@ -29,18 +29,40 @@ button {
     <br>
     <!-- penutup konten -->
     <h1 style="font-size: 17px; font-weight: 400;"> untuk versi Prem silahkan contact admin kami !!!</h1>
-    <br>
-    <div style="text-align: center;">
-      <a href="mailto:munirggplay@gmail.com" style="display: inline-block; margin-right: 10px;">
-        <img alt="Gmail Logo" height="32" src="https://www.gstatic.com/images/icons/material/product/2x/gmail_64dp.png" width="32" /> Email
-      </a>
-      <a href="https://wa.me/6283838367172" rel="noopener noreferrer" target="_blank" style="display: inline-block; margin-right: 10px;">
-        <img alt="WhatsApp logo" height="30" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png" width="30" /> WhatsApp
-      </a>
-      <a href="https://t.me/telgruptoppayofficial" target="_blank" style="display: inline-block;">
-        <img alt="Telegram" height="30" src="https://telegram.org/img/t_logo.svg" width="30" /> Telegram
-      </a>
-    </div>
+    <br><br>
+<div style="text-align: center;">
+  <!-- Tautan ke Media Sosial -->
+  <a href="https://www.facebook.com/akun-facebook-anda" target="_blank" style="display: inline-block; margin-right: 10px;">
+    <img alt="Facebook Logo" height="32" src="https://link.ke/gambar-logo-facebook" width="32" /> Facebook
+  </a>
+  <a href="https://www.instagram.com/akun-instagram-anda" target="_blank" style="display: inline-block; margin-right: 10px;">
+    <img alt="Instagram Logo" height="32" src="https://link.ke/gambar-logo-instagram" width="32" /> Instagram
+  </a>
+  <a href="https://www.tiktok.com/@akun-tiktok-anda" target="_blank" style="display: inline-block; margin-right: 10px;">
+    <img alt="TikTok Logo" height="32" src="https://link.ke/gambar-logo-tiktok" width="32" /> TikTok
+  </a>
+  <a href="https://www.snackvideo.com/@akun-snackvideo-anda" target="_blank" style="display: inline-block; margin-right: 10px;">
+    <img alt="SnackVideo Logo" height="32" src="https://link.ke/gambar-logo-snackvideo" width="32" /> SnackVideo
+  </a>
+  <a href="https://shopee.co.id/akun-shopee-anda" target="_blank" style="display: inline-block; margin-right: 10px;">
+    <img alt="Shopee Logo" height="32" src="https://link.ke/gambar-logo-shopee" width="32" /> Shopee
+  </a>
+  <a href="https://www.lazada.co.id/akun-lazada-anda" target="_blank" style="display: inline-block; margin-right: 10px;">
+    <img alt="Lazada Logo" height="32" src="https://link.ke/gambar-logo-lazada" width="32" /> Lazada
+  </a>
+
+  <!-- Tautan ke Kontak -->
+  <a href="mailto:munirggplay@gmail.com" style="display: inline-block; margin-right: 10px;">
+    <img alt="Gmail Logo" height="32" src="https://www.gstatic.com/images/icons/material/product/2x/gmail_64dp.png" width="32" /> Email
+  </a>
+  <a href="https://wa.me/6283838367172" rel="noopener noreferrer" target="_blank" style="display: inline-block; margin-right: 10px;">
+    <img alt="WhatsApp Logo" height="32" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png" width="32" /> WhatsApp
+  </a>
+  <a href="https://t.me/telgruptoppayofficial" target="_blank" style="display: inline-block;">
+    <img alt="Telegram Logo" height="32" src="https://telegram.org/img/t_logo.svg" width="32" /> Telegram
+  </a>
+</div>
+
     <h3 style="color: #3e005d;">
       <br /> We will revert you as soon as possible...!
     </h3>
