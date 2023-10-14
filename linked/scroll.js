7cde5eaa8e7be4943b105c926be02ab840a7ec09
MyHost <126472922+botstart@users.noreply.github.com>
2023-10-14 23:16:00 +0700

    Delete linked/scroll.js

--- linked/scroll.js
+++
@@ -1,8 +0,0 @@
-function autoScroll() {
-    window.scrollBy(0, 10); // Mengatur kecepatan scroll ke bawah
-    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
-        location.reload(); // Refresh halaman setelah mencapai bagian paling bawah
-    }
-}
-
-setInterval(autoScroll, 500); // Mengatur interval waktu scroll, contohnya 100 milidetik atau 0.1 detik
