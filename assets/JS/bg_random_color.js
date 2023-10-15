8d061f209a9c41528438e8542c051502341faa3b
MyHost <126472922+botstart@users.noreply.github.com>
2023-10-15 13:12:12 +0700

    Update bg_random_color.js

--- assets/JS/bg_random_color.js
+++ assets/JS/bg_random_color.js
@@ -1,26 +1,21 @@
+// Fungsi untuk mengatur warna latar belakang situs web dan #sidenav
+function setBackgroundColor(color) {
+  document.body.style.backgroundColor = color; // Atur latar belakang situs web
+  document.querySelector('#sidenav.active').style.backgroundColor = color; // Atur latar belakang #sidenav saat aktif
+  localStorage.setItem('backgroundColor', color);
+}
+
+// Fungsi untuk mendapatkan warna yang telah disimpan di localStorage
+function getSavedColor() {
+  return localStorage.getItem('backgroundColor');
+}
+
 // Fungsi untuk mendapatkan warna acak dalam format RGB
 function getRandomColor() {
   var r = Math.floor(Math.random() * 256);
   var g = Math.floor(Math.random() * 256);
   var b = Math.floor(Math.random() * 256);
   return 'rgb(' + r + ',' + g + ',' + b + ')';
-}
-
-// Fungsi untuk mendapatkan warna latar belakang dari localStorage
-function getSavedColor() {
-  return localStorage.getItem('backgroundColor');
-}
-
-// Fungsi untuk mengatur warna latar belakang untuk website dan #sidenav
-function setBackgroundColor(color) {
-  document.body.style.backgroundColor = color;
-  document.querySelector('#sidenav.active').style.backgroundColor = color;
-  localStorage.setItem('backgroundColor', color);
-}
-
-// Fungsi untuk menghapus warna latar belakang yang disimpan di localStorage
-function clearSavedColor() {
-  localStorage.removeItem('backgroundColor');
 }
 
 var savedColor = getSavedColor();
