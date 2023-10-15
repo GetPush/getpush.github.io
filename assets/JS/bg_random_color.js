c9819e77d30235a454e7ac508ff86e3f2146de4a
MyHost <126472922+botstart@users.noreply.github.com>
2023-10-15 13:05:47 +0700

    Update bg_random_color.js

--- assets/JS/bg_random_color.js
+++ assets/JS/bg_random_color.js
@@ -1,29 +1,33 @@
-        // Fungsi untuk mendapatkan warna acak dalam format RGB
-        function getRandomColor() {
-            var r = Math.floor(Math.random() * 256);
-            var g = Math.floor(Math.random() * 256);
-            var b = Math.floor(Math.random() * 256);
-            return 'rgb(' + r + ',' + g + ',' + b + ')';
-        }
+// Fungsi untuk mendapatkan warna acak dalam format RGB
+function getRandomColor() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
 
-        function getSavedColor() {
-            return localStorage.getItem('backgroundColor');
-        }
+// Fungsi untuk mendapatkan warna latar belakang dari localStorage
+function getSavedColor() {
+  return localStorage.getItem('backgroundColor');
+}
 
-        function setBackgroundColor(color) {
-            document.body.style.backgroundColor = color;
-            localStorage.setItem('backgroundColor', color); 
-        }
+// Fungsi untuk mengatur warna latar belakang untuk website dan #sidenav
+function setBackgroundColor(color) {
+  document.body.style.backgroundColor = color;
+  document.querySelector('#sidenav.active').style.backgroundColor = color;
+  localStorage.setItem('backgroundColor', color);
+}
 
-        function clearSavedColor() {
-            localStorage.removeItem('backgroundColor');
-        }
-        var savedColor = getSavedColor();
+// Fungsi untuk menghapus warna latar belakang yang disimpan di localStorage
+function clearSavedColor() {
+  localStorage.removeItem('backgroundColor');
+}
 
-        if (savedColor) {
-            setBackgroundColor(savedColor);
-        } else {
-            var randomColor = getRandomColor();
-            setBackgroundColor(randomColor);
-        }
-        clearSavedColor();
+var savedColor = getSavedColor();
+
+if (savedColor) {
+  setBackgroundColor(savedColor);
+} else {
+  var randomColor = getRandomColor();
+  setBackgroundColor(randomColor);
+}
