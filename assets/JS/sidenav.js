f9a0a23e16d274a79088832db6401e5afa2d1d5d
MyHost <126472922+botstart@users.noreply.github.com>
2023-10-15 17:14:24 +0700

    Update sidenav.js

--- assets/JS/sidenav.js
+++ assets/JS/sidenav.js
@@ -1,13 +1,15 @@
 
-		let burger = document.querySelector('.burger');
-		let close = document.querySelector('.close');
-		let sidenav = document.querySelector('#sidenav');
+  document.addEventListener("DOMContentLoaded", function() {
+    let burger = document.querySelector('.burger');
+    let close = document.querySelector('.close');
+    let sidenav = document.querySelector('#sidenav');
 
-		// Burger click function
-		burger.addEventListener('click', function () {
-			sidenav.classList.add('active');
-		});
-		// Close click function
-		close.addEventListener('click', function () {
-			sidenav.classList.remove('active');
-		});￼Enter
+    // Burger click function
+    burger.addEventListener('click', function () {
+      sidenav.classList.add('active');
+    });
+    // Close click function
+    close.addEventListener('click', function () {
+      sidenav.classList.remove('active');
+    });
+  });
