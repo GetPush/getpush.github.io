ee25820442babe0880f247ff72be7922f77bed6f
MyHost <126472922+botstart@users.noreply.github.com>
2023-10-17 01:00:40 +0700

    Update base.js

--- linked/base.js
+++ linked/base.js
@@ -71,7 +71,7 @@ head.appendChild(linkFavicon);
 // Buat elemen link untuk CSS Stylesheets
 var linkStylesheet = document.createElement('link');
 linkStylesheet.setAttribute('rel', 'stylesheet');
-linkStylesheet.setAttribute('href', '/linked/style.css');
+linkStylesheet.setAttribute('href', 'href="https://unpkg.com/ace-css/css/ace.min.css');
 head.appendChild(linkStylesheet);
 
 // Mendapatkan judul halaman
