cb87d4da74832b96a9aa6baabd9a8d48eb49dcb5
MyHost <126472922+botstart@users.noreply.github.com>
2023-10-16 12:13:48 +0700

    Update base.js

--- linked/base.js
+++ linked/base.js
@@ -168,6 +168,23 @@ const headerHTML = `
      </nav>    
    </div>  -->
 </header>
+<script>
+let burger = document.querySelector('.burger');
+let close = document.querySelector('.close');
+let sidenav = document.querySelector('#sidenav');
+
+// Burger click function
+burger.addEventListener('click', function () {
+  sidenav.style.left = '0';
+  document.body.style.overflow = 'hidden'; // Menghilangkan scroll pada badan dokumen
+});
+
+// Close click function
+close.addEventListener('click', function () {
+  sidenav.style.left = '-150%'; // Mengembalikan sidenav ke posisi semula
+  document.body.style.overflow = 'auto'; // Mengaktifkan kembali scroll pada badan dokumen
+});
+</script>
 <br>
 <br>
 <br>
