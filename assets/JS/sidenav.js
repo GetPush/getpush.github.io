37f21c0d09e65363239a1ce0f2d3d8ff0c21ca37
MyHost <126472922+botstart@users.noreply.github.com>
2023-10-16 12:12:32 +0700

    Update sidenav.js

--- assets/JS/sidenav.js
+++ assets/JS/sidenav.js
@@ -1,15 +1,15 @@
+let burger = document.querySelector('.burger');
+let close = document.querySelector('.close');
+let sidenav = document.querySelector('#sidenav');
 
-  document.addEventListener("DOMContentLoaded", function() {
-    let burger = document.querySelector('.burger');
-    let close = document.querySelector('.close');
-    let sidenav = document.querySelector('#sidenav');
+// Burger click function
+burger.addEventListener('click', function () {
+  sidenav.style.left = '0';
+  document.body.style.overflow = 'hidden'; // Menghilangkan scroll pada badan dokumen
+});
 
-    // Burger click function
-    burger.addEventListener('click', function () {
-      sidenav.classList.add('active');
-    });
-    // Close click function
-    close.addEventListener('click', function () {
-      sidenav.classList.remove('active');
-    });
-  });
+// Close click function
+close.addEventListener('click', function () {
+  sidenav.style.left = '-150%'; // Mengembalikan sidenav ke posisi semula
+  document.body.style.overflow = 'auto'; // Mengaktifkan kembali scroll pada badan dokumen
+});
