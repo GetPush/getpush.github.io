98c35c8fd9e7f8eb6b57a7ffac149035f8fbf94c
MyHost <126472922+botstart@users.noreply.github.com>
2023-10-21 21:34:05 +0700

    Delete linked directory

--- linked/shortlink/ouo.js
+++
@@ -1,9 +0,0 @@
-
-    var ouo_token = 'fTBXS1na';
-    var domains = ['safefileku.com']; 
-
-    (function() {
-        var script = document.createElement('script');
-        script.src = '//cdn.ouo.io/js/full-page-script.js';
-        document.getElementsByTagName('head')[0].appendChild(script);
-    })();
--- linked/shortlink/snacklink.js
+++
@@ -1,3 +0,0 @@
-    var go_url = 'https://besargaji.com/';
-    var api = 'a0ac298444f074ffe73a360a43ef78330dea10fd';
-    var shorten_includ = ["safefileku.com","up-load.io"];
